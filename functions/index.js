d970e6474498180eee879712527202cdec81d3ce
kiyohken2000 <www.broadwell.com@gmail.com>
2021-08-10 18:05:37 +0900

    master

--- functions/index.js
+++ functions/index.js
@@ -8,7 +8,7 @@ const expo = new Expo();
 
 exports.sendMessage = functions.region('asia-northeast2').firestore
   .document('live/{stream}')
-  .onCreate((snap, context) => {
+  .onCreate(async (snap, context) => {
     const newValue = snap.data();
     const createdUser = newValue.createdUser
     const name = createdUser.fullName
@@ -17,19 +17,18 @@ exports.sendMessage = functions.region('asia-northeast2').firestore
     for (const elem of follower) {
       const message = []
       const userRef = db.collection('tokens').doc(elem)
-      userRef.get().then((doc) => {
-        if (doc.exists) {
-          const data = doc.data()
-          const token = data.token
-          message.push({
-            to: token,
-            sound: 'default',
-            title: name,
-            body: 'going on the air.'
-          })
-          console.log(message)
-          expo.sendPushNotificationsAsync(message)
-        } else { null }
-      })
+      const doc = await userRef.get();
+      if (doc.exists) {
+        const data = doc.data();
+        const token = data.token;
+        message.push({
+          to: token,
+          sound: 'default',
+          title: name,
+          body: 'going on the air.'
+        });
+        console.log(message);
+        expo.sendPushNotificationsAsync(message);
+      } else { null; }
     }
   });
